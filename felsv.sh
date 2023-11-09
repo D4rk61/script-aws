3b214b326815e59805c2b6a9bb1ab5bfb3bfcca8
Jose <reynosojose2005@gmail.com>
2023-11-08 21:19:25 -0600

    test15

--- felsv.sh
+++ felsv.sh
@@ -28,18 +28,20 @@ export DEBIAN_FRONTEND=noninteractive
 
 back_s="backend_session"
 front_s="frontend_session"
-consiti_felsv_back_url="https://felsv.s3.us-east-2.amazonaws.com/consiti-felsv.zip"
+consiti_felsv_back_url="https://felsv.s3.us-east-2.amazonaws.com/consiti-felsv-new.zip"
 env_template_url="https://felsv.s3.us-east-2.amazonaws.com/env-template"
 # Declaracion de funciones
 function control-errores() {
     if [ $? -ne 0 ]; then
         touch log-error.txt
         echo "Error de ejecucion! en la linea $LINENO" >> log-error.txt
+        echo "Comando que falló: $BASH_COMMAND" >> log-error.txt
+        echo "Código de salida: $?" >> log-error.txt
     fi
 }
 
 function front-dev() {
-    echo -e "\e[31mActivando front dev\e[0m"
+    echo -e "\e[32mActivando front dev\e[0m"
     tmux send-keys -t $front_s "git clone https://github.com/D4rk61/ReactJS-login.git" C-m
     tmux send-keys -t $front_s "mv ReactJS-login consiti-felsv-frontend" C-m
     tmux send-keys -t $front_s "sleep 2" C-m
@@ -50,7 +52,7 @@ function front-dev() {
 }
 
 function front-prod() {
-    echo -e "\e[31mActivando front prod\e[0m"
+    echo -e "\e[32mActivando front prod\e[0m"
     tmux send-keys -t $front_s "git clone https://github.com/D4rk61/ReactJS-login.git" C-m
     tmux send-keys -t $front_s "mv ReactJS-login consiti-felsv-frontend" C-m
     tmux send-keys -t $front_s "sleep 2" C-m
@@ -61,7 +63,7 @@ function front-prod() {
 }
 
 function back-dev() {
-    echo -e "\e[31mActivando back dev\e[0m"
+    echo -e "\e[32mActivando back dev\e[0m"
     tmux send-keys -t $back_s "mv consiti-felsv consiti-felsv-backend" C-m
     tmux send-keys -t $back_s "sleep 2" C-m
     tmux send-keys -t $back_s "mv env-template consiti-felsv-backend" C-m
@@ -77,7 +79,7 @@ function back-dev() {
 }
 
 function back-prod() {
-    echo -e "\e[31mActivando back prod\e[0m"
+    echo -e "\e[32mActivando back prod\e[0m"
     tmux send-keys -t $back_s "mv consiti-felsv consiti-felsv-backend" C-m
     tmux send-keys -t $back_s "mv env-template consiti-felsv-backend" C-m
     tmux send-keys -t $back_s "chmod 777 -R consiti-felsv-backend" C-m
@@ -92,16 +94,16 @@ function back-prod() {
 }
 
 function create-tmux-sessions() {
-    echo -e "\e[31mCreando sessiones tmux\e[0m"
+    echo -e "\e[32mCreando sessiones tmux\e[0m"
     tmux start-server
     tmux new-session -d -s $back_s
     tmux new-session -d -s $front_s
-    echo -e "\e[31mSessiones creadas!\e[0m"
+    echo -e "\e[32mSessiones creadas!\e[0m"
 
 }
 
 function prepair_env() {
-    echo -e "\e[31mPreparando entorno y wget\e[0m"
+    echo -e "\e[32mPreparando entorno y wget\e[0m"
 
     mkdir felsv-project
     cd felsv-project
@@ -112,24 +114,23 @@ function prepair_env() {
 
 
 function tool-install() {
-    echo -e "\e[31mInstalando lo necesario\e[0m"
+    echo -e "\e[32mInstalando lo necesario\e[0m"
 
     # Instalacion de herramientas
-    DEBIAN_FRONTEND=noninteractive apt update -y ; apt install -y git unzip ca-certificates curl gnupg npm make tmux || control-errores
+    DEBIAN_FRONTEND=noninteractive apt update -y ; apt install -y git unzip ca-certificates curl gnupg npm make tmux xz-utils aptitude || control-errores
     DEBIAN_FRONTEND=noninteractive sudo install -m 0755 -d /etc/apt/keyrings || control-errores
 
-    # Instalacion de nvm
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-    source ~/.nvm/nvm.sh
-    nvm install 21
-    nvm use 21 --default
-
+    # Instalacion de nodejs
+    node-install-funct
+    echo -e "\e[32mInstalacion completa de nodejs\e[0m"
     # Llamada a la instalacion de docker
     docker-install-funct
+    echo -e "\e[32mInstalacion completa de docker-compose\e[0m"
+
 }
 
 function docker-install-funct() {
-    echo -e "\e[31mInstalando docker\e[0m"
+    echo -e "\e[32mInstalando docker\e[0m"
 
     # Instalacion de docker
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -148,43 +149,52 @@ function docker-install-funct() {
     docker-compose-install-funct
 
     # Llamada a la post-instalacion de docker
-    echo -e "\e[31mInstalacion de docker exitosa\e[0m"
+    echo -e "\e[32mInstalacion de docker exitosa\e[0m"
 
     docker-post-install-funct
 }
 
 
 function docker-compose-install-funct() {
-    sudo curl -L "https://github.com/docker/compose/releases/download/$desired_version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-    /usr/local/bin/docker-compose 
-    echo -e "\e[31mFinalizacion de instalacion de docker-compose\e[0m"
+    DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+    mkdir -p $DOCKER_CONFIG/cli-plugins
+    curl -SL https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+    chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+    echo -e "\e[32mFinalizacion de instalacion de docker-compose\e[0m"
 }
 
 
 function docker-post-install-funct() {
-    echo -e "\e[31mComenzando post-instalacion de docker\e[0m"
+    echo -e "\e[32mComenzando post-instalacion de docker\e[0m"
 
     # Post-instalacion de docker
 
-    echo -e "\e[31mCreando grupo docker\e[0m"
+    echo -e "\e[32mCreando grupo docker\e[0m"
     if ! getent group docker >/dev/null; then
         sudo groupadd docker
     fi
-    echo -e "\e[31mFinalizacion de creacion grupo docker y asignando el grupo docker\e[0m"
+    echo -e "\e[32mFinalizacion de creacion grupo docker y asignando el grupo docker\e[0m"
     usermod -aG docker $USER
     #echo -e "\e[31ejecutando newgrp\e[0m"
     #newgrp docker
     #echo -e "\e[31finalizando newgrp\e[0m"
-    echo -e "\e[31mFinalizacion de post-instalacion de docker\e[0m"
+    echo -e "\e[32mFinalizacion de post-instalacion de docker\e[0m"
 
 
     # Cargar la configuración en el shell actual
     #source /etc/environment
 }
 
+function node-install-funct() {
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+    . ~/.bashrc
+    nvm install 20
+    curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+    DEBIAN_FRONTEND=noninteractive apt install nodejs -y
+}
+
 function activate-dev-profile() {
-    echo -e "\e[31mActivando perfil dev\e[0m"
+    echo -e "\e[32mActivando perfil dev\e[0m"
 
     # Llamada al backend
     back-dev
@@ -193,7 +203,7 @@ function activate-dev-profile() {
     front-dev
 }
 function activate-prod-profile() {
-    echo -e "\e[31mActivando perfil prod\e[0m"
+    echo -e "\e[32mActivando perfil prod\e[0m"
 
     # Llamada al backend
     back-prod
